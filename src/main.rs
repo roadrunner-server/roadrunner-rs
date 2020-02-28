acd0636c055b6331a11d426faed8ee8956ac5efb
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-02-29 00:32:21 +0300

    Add configs, initial structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{App, AppSettings, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::VersionlessSubcommands)
+        .subcommand(SubCommand::with_name("serve").about("run RR"))
+        .get_matches();
+
+    match cli.subcommand() {
+        ("serve", Some(matches)) => {
+            println!("wooooow");
+        }
+
+        _ => unreachable!(),
+    }
 }
