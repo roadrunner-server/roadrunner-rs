a9c34d64d9af89b597677addcd05f5f5ac1d13e0
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-04-02 16:00:31 +0300

    Add frame_reading
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- crates/goridge/src/frame/mod.rs
+++ crates/goridge/src/frame/mod.rs
@@ -1,5 +1,9 @@
-use std::convert::{TryFrom, TryInto};
+use std::convert::{TryInto};
 use crate::errors::Error;
+use std::ops::BitAnd;
+
+const WORD: u8 = 4;
+const FRAME_OPTIONS_MAX_SIZE: u8 = 40;
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
 struct Frame {
@@ -17,14 +21,17 @@ impl Frame {
         f
     }
 
+    #[inline]
     fn write_hl(&mut self, hl: u8) {
         self.header[0] |= hl;
     }
 
+    #[inline]
     fn default_hl(&mut self) {
         self.write_hl(3);
     }
 
+    #[inline]
     fn read_header(&self, data: &[u8]) -> Result<Self, Error> {
         if data.len() < 12 {
             return Err(Error::HeaderLenError { cause: "len is less than 12".to_string() });
@@ -33,6 +40,28 @@ impl Frame {
             header: data[..12].try_into().expect("slice with incorrect length"),
             payload: vec![],
         })
+    }
+
+    fn read_frame(&self, data: &[u8]) -> Self {
+        // get options bits
+        let opt = data[0].bitand(0x0F);
+
+        if opt > 3 {
+            return Self {
+                header: data[..(opt * WORD) as usize].try_into().expect("array with incorrect length"),
+                payload: vec![],
+            };
+        }
+
+        let mut frame = Frame {
+            header: data[..12_usize].try_into().expect("array with incorrect length"),
+            payload: data[12_usize..].try_into().expect("can't transform slice into vector"),
+        };
+
+        frame.header[10] = 0;
+        frame.header[11] = 0;
+
+        frame
     }
 }
 
