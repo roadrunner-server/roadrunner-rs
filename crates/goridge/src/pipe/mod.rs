93f0392f4ac13516cfcc30738574179727b97a25
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-10-21 22:39:28 +0300

    edition 2021
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- crates/goridge/src/pipe/mod.rs
+++ crates/goridge/src/pipe/mod.rs
@@ -2,24 +2,30 @@ use crate::errors::Error;
 use crate::frame::Frame;
 use crate::relay::Relay;
 use std::io::{BufReader, Read, Write};
-use std::process::{Command, Stdio};
+use std::process::{ChildStderr, ChildStdin, ChildStdout, Command, Stdio};
 
 pub struct Pipes {
-    stdout: Box<dyn Write>,
-    stdin: Box<dyn Read>,
+    // stdout: Box<dyn Write>,
+    stderr: Box<dyn Write>,
+    // stdin: Box<dyn Read>,
+    // stderr: Option<ChildStderr>,
+    // stdin: Option<ChildStdin>,
+    // stdout: Option<ChildStdout>,
 }
 
 impl Relay<Frame> for Pipes {
     fn send(&mut self, frame: &mut Frame) -> Result<(), Error> {
         let v: Vec<u8> = frame.into();
-        self.stdout.write_all(&v[..])?;
+        // self.stdin.unwrap().write_all(&v[..]);
+
+        // self.stdout.unwrap().write_all(&v[..])?;
         Ok(())
     }
 
     fn receive(&mut self) -> Result<Vec<u8>, Error> {
-        let mut reader = BufReader::new(&mut self.stdin);
+        // let mut reader = BufReader::new(&mut self.stdin);
         let mut data: Vec<u8> = vec![];
-        reader.read_to_end(&mut data)?;
+        // reader.read_to_end(&mut data)?;
         Ok(data)
     }
 
@@ -33,13 +39,36 @@ impl Relay<Frame> for Pipes {
 }
 
 impl Pipes {
-    fn new(cmd: &str) -> Self {
+    fn new(cmd: &str, stderr: Box<dyn std::io::Write>) -> Result<Self, std::io::Error> {
+        // let mut command = Command::new(cmd)
+        //     .stdin(Stdio::piped())
+        //     .stdout(Stdio::piped())
+        //     .stderr(Stdio::piped())
+        //     .spawn()?;
+        //
+        Ok(Pipes {
+            stderr: stderr,
+            // stdout: command.stdout,
+            // stdin: command.stdin,
+        })
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::process::{Command, Stdio};
+    use crate::pipe::Pipes;
+
+    #[test]
+    fn test1() {
+        let cmd: &str = "ls";
         let mut command = Command::new(cmd)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
-            .spawn();
+            .spawn().unwrap();
 
-        Pipes {}
+        Pipes::new("ls", Box::new(command.stderr.into()));
     }
 }
